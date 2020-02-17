d2d0573863786ef48e59e9c7fed630ed3f50cbfa
mkmk884 <maki884.8827@gmail.com>
2020-02-18 03:48:30 +0900

    学習用のブログサイトの作成

---
+++ myblog/app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: 'desc')
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    # render plain: params[:post].inspect
+    # save
+    # @post = Post.new(params[:post])
+    @post = Post.new(post_params)
+    if @post.save
+      # redirect
+      redirect_to posts_path
+    else
+      render plain: @post.errors.inspect
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
+end
